eb38fe7347c862a2d8bee62c6115d77be080e1bb
Blair Prescott <Blair.Prescott@broadcom.com>
2020-12-10 11:51:16 -0800

    Allow all devices to configure LOHS for 5/6GHz
    Some non-auto devices don't support 2.4GHz wifi. Allow any device
    to be configured for 5GHz or 6GHz bands for local-only hotspot.
    
    Bug: 175333688
    
    Test: VtsHalWifiHostapdV1_2TargetTest on cypress
    Change-Id: I913139fef8c51bbeeb9c248ee7c31b987cf15158

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -1455,17 +1455,14 @@ public class WifiServiceImpl extends BaseWifiService {
         private void startForFirstRequestLocked(LocalOnlyHotspotRequestInfo request) {
             int band = SoftApConfiguration.BAND_2GHZ;
 
-            // For auto only
-            if (hasAutomotiveFeature(mContext)) {
-                if (mContext.getResources().getBoolean(R.bool.config_wifiLocalOnlyHotspot6ghz)
-                        && mContext.getResources().getBoolean(R.bool.config_wifiSoftap6ghzSupported)
-                        && is6GhzBandSupportedInternal()) {
-                    band = SoftApConfiguration.BAND_6GHZ;
-                } else if (mContext.getResources().getBoolean(
-                        R.bool.config_wifi_local_only_hotspot_5ghz)
-                        && is5GhzBandSupportedInternal()) {
-                    band = SoftApConfiguration.BAND_5GHZ;
-                }
+            if (mContext.getResources().getBoolean(R.bool.config_wifiLocalOnlyHotspot6ghz)
+                    && mContext.getResources().getBoolean(R.bool.config_wifiSoftap6ghzSupported)
+                    && is6GhzBandSupportedInternal()) {
+                band = SoftApConfiguration.BAND_6GHZ;
+            } else if (mContext.getResources().getBoolean(
+                    R.bool.config_wifi_local_only_hotspot_5ghz)
+                    && is5GhzBandSupportedInternal()) {
+                band = SoftApConfiguration.BAND_5GHZ;
             }
 
             SoftApConfiguration softApConfig = WifiApConfigStore.generateLocalOnlyHotspotConfig(
