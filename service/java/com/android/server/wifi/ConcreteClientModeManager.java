fde4aec4a369c4d0055a4d6e29bc991f43e5afb6
Wenjia Jiang <wenjia.jiang@mediatek.com>
2021-10-13 20:26:41 +0800

    Add the wifi dongle hot-plug mechanism
    [Description]
    When the expected power off/on or pull out/insertion occurs
    on the WiFi Dongle(not wifinative failure), Wifi Recovery
    can be triggered by monitoring the message of the interface
    added from NetdWapper.
    
    Cherry-picked from aosp/1864817.
    
    [Test Report]
    Build pass
    Functional validation pass
    
    Bug: 138556447
    
    Test: atest WifiNativeTest SelfRecoveryTest WifiNativeInterfaceManagementTest ConcreteClientModeManagerTest
    Merged-In: I9ae4ff1c853187e039c7c714d58e3312a77cb133
    Change-Id: Ic474b962ea18be50ed9a3d30872d1ee26e3a0e6e

--- service/java/com/android/server/wifi/ConcreteClientModeManager.java
+++ service/java/com/android/server/wifi/ConcreteClientModeManager.java
@@ -62,6 +62,7 @@ import com.android.internal.util.State;
 import com.android.internal.util.StateMachine;
 import com.android.modules.utils.HandlerExecutor;
 import com.android.server.wifi.WifiNative.InterfaceCallback;
+import com.android.server.wifi.WifiNative.InterfaceEventCallback;
 import com.android.server.wifi.WifiNative.RxFateReport;
 import com.android.server.wifi.WifiNative.TxFateReport;
 import com.android.server.wifi.util.ActionListenerWrapper;
@@ -699,6 +700,7 @@ public class ConcreteClientModeManager implements ClientModeManager {
         public static final int CMD_INTERFACE_DESTROYED = 4;
         public static final int CMD_INTERFACE_DOWN = 5;
         public static final int CMD_SWITCH_TO_SCAN_ONLY_MODE_CONTINUE = 6;
+        public static final int CMD_INTERFACE_ADDED = 7;
         private final State mIdleState = new IdleState();
         private final State mStartedState = new StartedState();
         private final State mScanOnlyModeState = new ScanOnlyModeState();
@@ -710,6 +712,39 @@ public class ConcreteClientModeManager implements ClientModeManager {
 
         @Nullable
         private StateMachineObituary mObituary = null;
+
+        private final InterfaceEventCallback mWifiNativeInterfaceEventCallback =
+                new InterfaceEventCallback() {
+
+            boolean mEnabling = false;
+
+            @Override
+            public void onInterfaceLinkStateChanged(String ifaceName, boolean isLinkUp) {
+                Log.d("InterfaceEventCallback",
+                        "onInterfaceLinkStateChanged, ifaceName=" + ifaceName + " up="
+                                + isLinkUp + " CurrentState=" + getCurrentStateName());
+                if (isLinkUp) {
+                    mEnabling = false;
+                }
+            }
+
+            @Override
+            public void onInterfaceAdded(String ifaceName) {
+                String currentSta = getCurrentStateName();
+                Log.d("InterfaceEventCallback",
+                        "onInterfaceAdded, ifaceName=" + ifaceName + " CurrentState=" + currentSta);
+                if (mStateMachine.getCurrentState() == null) {
+                    Log.d(TAG, "StateMachine not active, trigger ifaceAddedDetected");
+                    mSelfRecovery.trigger(SelfRecovery.REASON_IFACE_ADDED);
+                } else if (!mEnabling) {
+                    Log.d("InterfaceEventCallback", "send CMD_INTERFACE_ADDED");
+                    mStateMachine.sendMessage(CMD_INTERFACE_ADDED);
+                    mEnabling = true;
+                } else {
+                    Log.d("InterfaceEventCallback", "wifi already in the start");
+                }
+            }
+        };
 
         private final InterfaceCallback mWifiNativeInterfaceCallback = new InterfaceCallback() {
             @Override
@@ -848,6 +883,8 @@ public class ConcreteClientModeManager implements ClientModeManager {
                         RoleChangeInfo roleChangeInfo = (RoleChangeInfo) message.obj;
                         mClientInterfaceName = mWifiNative.setupInterfaceForClientInScanMode(
                                 mWifiNativeInterfaceCallback, roleChangeInfo.requestorWs);
+                        mWifiNative.setWifiNativeInterfaceEventCallback(
+                                mWifiNativeInterfaceEventCallback);
                         if (TextUtils.isEmpty(mClientInterfaceName)) {
                             Log.e(getTag(), "Failed to create ClientInterface. Sit in Idle");
                             takeBugReportInterfaceFailureIfNeeded(
@@ -862,6 +899,10 @@ public class ConcreteClientModeManager implements ClientModeManager {
                             mScanRoleChangeInfoToSetOnTransition = roleChangeInfo;
                             transitionTo(mScanOnlyModeState);
                         }
+                        break;
+                    case CMD_INTERFACE_ADDED:
+                        Log.d(getTag(), "IdleState received CMD_INTERFACE_ADDED");
+                        mSelfRecovery.trigger(SelfRecovery.REASON_IFACE_ADDED);
                         break;
                     default:
                         Log.d(getTag(), "received an invalid message: " + message);
