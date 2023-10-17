0088353431d4a7a3f9ac8d9bdfb1442db6d4f47a
Rashmi Kumari <kumarashmi@google.com>
2023-10-17 06:58:05 +0000

    Framework changes to mock pno scan and wifi scan
    Change-Id: If8b78de3c2d746d89d5a423309ea133ec91f930c

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -7159,7 +7159,9 @@ public class WifiServiceImpl extends BaseWifiService {
     }
 
     private boolean isPnoSupported() {
-        return (getSupportedFeatures() & WifiManager.WIFI_FEATURE_PNO) != 0;
+       // return (getSupportedFeatures() & WifiManager.WIFI_FEATURE_PNO) != 0;
+    /* TBD - Returning true to support Pno Scan for CF */
+        return true;
     }
 
     /**
